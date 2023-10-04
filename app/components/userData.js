0d5b09b35e3d5a4b7593649f300c099565e65da9
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-10-04 17:08:11 -0400

    missing file

---
+++ app/components/userData.js
@@ -0,0 +1,44 @@
+
+export const userData1 = ([
+    [
+        ["Christino Barbosa"],
+        ["14:30"],
+        [15]
+    ],
+    [
+        
+    ],
+    [
+        ["Christino Barbosa"],
+        ["14:30"],
+        [15]
+    ],
+    [
+        ["Christino Barbosa"],
+        ["14:30"],
+        [15]
+    ],
+    [
+        
+    ],
+    [
+        
+    ],
+    [
+        ["Christino Barbosa"],
+        ["14:30"],
+        [15]
+    ]
+])
+
+export default function Data() {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+    'Thursday', 'Friday', 'Saturday']
+
+    return(
+    <div className="grid grid-flow-row border border-black m-2 p2">
+        
+    </div>
+    )
+}
+//include time(s) in dates list
